4819c98d9d5a21bc28f2b93379c599af4fbf179b
PettTo <t.pett@tu-braunschweig.de>
2021-06-15 12:57:33 +0200

    add algorithms

---
+++ algorithms/tools/saobExecution.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+#java -da -Xmx4g -Xms2g -Xloggc:'A:/210_Research/206_AutoSMP_SRC_Repo/output/temp/gcCollector.txt' -cp 'A:/210_Research/206_AutoSMP_SRC_Repo/algorithms/tools/' -jar saob.jar
+
+echo "funktioniert das"
+java -da -Xmx4g -Xms2g -cp A:/210_Research/206_AutoSMP_SRC_Repo/algorithms/tools/ -jar saob.jar
+#java -da -Xmx4g -Xms2g -cp A:/210_Research/206_AutoSMP_SRC_Repo/algorithms/tools/ -jar saob.jar
